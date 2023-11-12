ebdea8c4aae44102d1a2a291e890ce2acb9cf97a
Для Всех <abracadabra@gmail.com>
2023-11-12 19:46:36 +0200

    ver 2

--- cashbacker.py
+++ cashbacker.py
@@ -24,19 +24,3 @@ for j in range (1,7):
             with open("cashback.txt", "a", encoding="utf-8") as file:
                 file.write(f"{cashbacks_prod}\n")
             # print(cashbacks.text)
-
-
-# if response.status_code == 200:
-#     soup = BeautifulSoup(response.text, "lxml")
-#     companies = soup.find_all("div", class_="col-lg-2 col-md-3 shop-list-card pseudo-link no-link")
-#     # print(len(companies))
-#
-#
-# for prod in companies:
-#     if prod.find('div', class_="card mb-4"):
-#         cashbacks = prod.find('div', class_="card-body")
-#         # cashbacks_prod = cashbacks.text
-#         # with open("cashback.txt", "a", encoding="utf-8") as file:
-#         #     file.write(f"{cashbacks_prod}\n")
-#         print(cashbacks.text)
-
